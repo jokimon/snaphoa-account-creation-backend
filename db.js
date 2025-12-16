feab5100860c5e73d8321601c70d4f84330bc48e
Joon Kim <joonkim@Joons-MBP.lan>
2025-12-16 00:56:49 -0800

    update with mongodb connection

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    console.log("MONGO_URI =", process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI); // <-- no extra options
+    console.log("MongoDB connected!");
   } catch (err) {
-    console.error('MongoDB connection failed', err);
+    console.error("MongoDB connection error:", err);
     process.exit(1);
   }
 };
