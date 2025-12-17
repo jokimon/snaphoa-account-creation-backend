2b125b2fa3f4384fa68420f82ed7d72991a6b741
Joon Kim <joonkim@Joons-MacBook-Pro.local>
2025-12-16 23:25:21 -0800

    mongodb atlas connection

--- db.js
+++ db.js
@@ -2,17 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI not defined in environment variables!");
-    }
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    // Make sure your URI includes the database name, e.g., 'hoaApp'
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(`MongoDB connected: ${conn.connection.host}, database: ${conn.connection.name}`);
+    console.log(
+      `MongoDB connected: ${conn.connection.host}, DB: ${conn.connection.name}`
+    );
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
